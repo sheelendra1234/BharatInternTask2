c2371c79f1e087611a2951bf63d9ca9363983a88
sheelendra1234 <158015749+sheelendra1234@users.noreply.github.com>
2024-02-07 09:28:53 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navlinks = document.querySelector('.nav-links');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlinks.classList.toggle('open')
-}
